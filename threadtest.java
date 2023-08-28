85ca20ebfd6857d9b3e18e470a5562393736e0c9
bing <ruby090215@gmail.com>
2023-08-28 21:05:35 +0800

    bing0828

--- threadtest.java
+++ threadtest.java
@@ -12,6 +12,7 @@ class ThreadTest {
             public void run(){
                 increment();
                 decrement();
+                pow(3);
                 System.out.println("runnable");
             }
         };
@@ -26,6 +27,16 @@ class ThreadTest {
 
     public static void decrement(){
         count--;
+    }
+
+
+    public static void pow(int i){
+        int ocount = count;
+        for(int j=1;j<i;j++){
+            
+            int count2 = count * ocount;
+            count = count2;
+        }
     }
 }
 
